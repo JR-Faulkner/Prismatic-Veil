4d1e846c67c76c562c9ec9b4788a365052ca2a72
JR-Faulkner <53619182+JR-Faulkner@users.noreply.github.com>
2026-09-01 17:11:00 -0400

    live26: connect production Auryi FX driver to formation

---
+++ src/prizim/Live26PartyFormationView.js
@@ -0,0 +1,12 @@
+// LIVE26 formation bridge.
+// Keeps live25 formation/crown alignment while swapping in the production
+// Auryi FX sequence driver.
+import Live25PartyFormationView from './Live25PartyFormationView.js?v=live25';
+import Live26DuoHybridSequenceDriver from './Live26DuoHybridSequenceDriver.js?v=live26';
+
+export default class Live26PartyFormationView extends Live25PartyFormationView {
+  constructor(scene) {
+    super(scene);
+    this.duoHybrid = new Live26DuoHybridSequenceDriver(scene);
+  }
+}
