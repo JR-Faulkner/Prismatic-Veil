e90f2281d19795013304f11d29ddc4f2d5ec7373
JR-Faulkner <53619182+JR-Faulkner@users.noreply.github.com>
2026-09-01 18:50:06 -0400

    LIVE26F: bump Auryi PNG driver cache

--- src/prizim/Live26PartyFormationView.js
+++ src/prizim/Live26PartyFormationView.js
@@ -2,7 +2,7 @@
 // Keeps live25 formation/crown alignment while swapping in the production
 // Auryi FX sequence driver.
 import Live25PartyFormationView from './Live25PartyFormationView.js?v=live25';
-import Live26DuoHybridSequenceDriver from './Live26DuoHybridSequenceDriver.js?v=live26e';
+import Live26DuoHybridSequenceDriver from './Live26DuoHybridSequenceDriver.js?v=live26f';
 
 export default class Live26PartyFormationView extends Live25PartyFormationView {
   constructor(scene) {
